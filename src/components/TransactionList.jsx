7de36b68ebf2c530ae8928bcb8a3bc47fc7a3e99
Alex N <alexneguleanu@gmail.com>
2025-05-28 20:24:02 +0000

    TransactionList component

--- src/components/TransactionList.jsx
+++ src/components/TransactionList.jsx
@@ -1,7 +1,25 @@
 import React from "react";
 
-const TransactionList = () => {
-  return <div>TransactionList</div>;
+const TransactionList = ({ items }) => {
+  console.log("Items received: ", items);
+  return (
+    <div>
+      {items.map((item) => (
+        <div
+          key={item.id}
+          className="border-2 group relative flex  s p-2 mb-2 shadow-md"
+        >
+          <button className="absolute left-2 opacity-0 group-hover:opacity-100 transition">
+            🗑️
+          </button>
+          <div className="flex flex-1 justify-around gap-4">
+            <p>{item.description}</p>
+            <p>${item.amount}</p>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default TransactionList;
