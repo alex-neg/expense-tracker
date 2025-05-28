61149820b410a0de01d10524ea38b46f9e407644
Alex N <alexneguleanu@gmail.com>
2025-05-28 20:25:20 +0000

    Created addTransaction functionality

--- src/components/ExpenseControls.jsx
+++ src/components/ExpenseControls.jsx
@@ -1,18 +1,55 @@
-import React from "react";
+import { React, useState, useRef } from "react";
+import TransactionList from "./TransactionList";
 
 const ExpenseControls = () => {
+  const [transactions, setTransactions] = useState([]);
+
+  // will need to refactor later and use useState for accesing the inputs
+  const descriptionRef = useRef();
+  const amountRef = useRef();
+
+  const addTransaction = (e) => {
+    e.preventDefault();
+
+    const description = descriptionRef.current.value;
+    const amount = amountRef.current.value;
+    if (!description.trim() || !amount.trim()) return;
+    const newTransaction = {
+      id: Date.now(),
+      description: description,
+      amount: Number(amount),
+    };
+    setTransactions((prev) => {
+      return [...prev, newTransaction];
+    });
+    descriptionRef.current.value = "";
+    amountRef.current.value = "";
+    console.log(newTransaction);
+  };
+
   return (
     <div className="mt-20 border-2">
-      <form>
+      <form onSubmit={addTransaction}>
         <h1>Add new transaction</h1>
         <div className="p-4 gap-4 flex flex-col items-center justify-center">
-          <input className="w-46" type="text" placeholder="Enter description" />
-          <input className="w-46" type="text" placeholder="Enter amount" />
+          <input
+            ref={descriptionRef}
+            className="w-46"
+            type="text"
+            placeholder="Enter description"
+          />
+          <input
+            ref={amountRef}
+            className="w-46"
+            type="text"
+            placeholder="Enter amount"
+          />
           <button className="border-2 w-46" type="submit">
             Add
           </button>
         </div>
       </form>
+      <TransactionList items={transactions} />
     </div>
   );
 };
