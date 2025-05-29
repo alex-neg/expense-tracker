e8d0b800175f5e13d9a4830c6a38b61f7158d058
Alex N <alexneguleanu@gmail.com>
2025-05-29 18:19:41 +0000

    Styled the component

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,10 @@ import ExpenseControls from "./components/ExpenseControls";
 function App() {
   return (
     <div className="flex justify-center items-center min-h-screen bg-gray-700">
-      <div className="border-4 border-slate-900 bg-gray-300 w-11/12 max-w-md min-h-[600px] p-8 m-8 rounded-xl">
-        <h1 className="text-2xl font-bold my-4">Expense Tracker</h1>
-        <BalanceDisplay />
+      <div className=" bg-gray-300 w-11/12 max-w-md min-h-[600px] p-8 m-8 rounded-xl">
+        <h1 className="text-center text-2xl text-slate-900 font-bold m-4">
+          Expense Tracker
+        </h1>
         <ExpenseControls />
       </div>
     </div>
