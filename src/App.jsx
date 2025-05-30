6a7a36a4a78394f67d4c7cc1be941d71c4624a15
Alex N <alexneguleanu@gmail.com>
2025-05-30 16:13:33 +0000

    Updated min-h-screen

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import ExpenseControls from "./components/ExpenseControls";
 function App() {
   return (
     <div className="flex justify-center items-center min-h-screen bg-gray-700">
-      <div className=" bg-gray-300 w-11/12 max-w-md min-h-[600px] p-8 m-8 rounded-xl">
+      <div className=" bg-gray-300 w-11/12 max-w-md min-h-[500px] p-8 m-8 rounded-xl">
         <h1 className="text-center text-2xl text-slate-900 font-bold m-4">
           Expense Tracker
         </h1>
